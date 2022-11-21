d7972e6ed2c36ba25f0ad77bbb7e6505fff87ba1
LinkLeong <a624669980@163.com>
2022-11-21 07:41:57 +0000

    wip

--- route/api_route_event.go
+++ route/api_route_event.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"time"
@@ -87,9 +88,18 @@ func (r *APIRoute) PublishEvent(ctx echo.Context, sourceID codegen.SourceID, nam
 	}
 
 	var properties map[string]string
-	if err := ctx.Bind(&properties); err != nil {
+	body, err := ioutil.ReadAll(ctx.Request().Body)
+
+	if err != nil {
+
 		message := err.Error()
 		return ctx.JSON(http.StatusBadRequest, codegen.ResponseBadRequest{Message: &message})
+	} else {
+		err = json.Unmarshal(body, &properties)
+		if err != nil {
+			message := err.Error()
+			return ctx.JSON(http.StatusBadRequest, codegen.ResponseBadRequest{Message: &message})
+		}
 	}
 
 	event := codegen.Event{
